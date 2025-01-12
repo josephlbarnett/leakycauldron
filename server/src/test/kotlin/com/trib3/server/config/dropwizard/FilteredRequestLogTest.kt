11c4f16a790011ea7d1ef4972c922c563117428e
Joe Barnett <joseph.levi.barnett@gmail.com>
2025-01-12 14:29:50 -0800

    Port to Dropwizard 5/Jetty 12 [BREAKING CHANGE]
    Maven and java packages changed to ee10 versions of things.
    Websocket API changes invovling callbacks/etc, also the upgrade
    process changes to deal with jetty Request vs/ servlet requests.
    Similar mock/logging changes for those request APIs.
    Adopt https://github.com/dropwizard/dropwizard/pull/9970 to avoid
    errors in request logs.
    
    Downstreams might be ok, but API changes ae major enough that
    they might not be so bump version and label as breaking change.

--- server/src/test/kotlin/com/trib3/server/config/dropwizard/FilteredRequestLogTest.kt
+++ server/src/test/kotlin/com/trib3/server/config/dropwizard/FilteredRequestLogTest.kt
@@ -10,13 +10,17 @@ import ch.qos.logback.classic.Logger
 import ch.qos.logback.core.AppenderBase
 import com.google.common.collect.ImmutableList
 import com.trib3.testing.LeakyMock
+import com.trib3.testing.server.TestServletContextRequest
 import io.dropwizard.logging.common.AppenderFactory
 import jakarta.servlet.http.HttpServletResponse
 import org.easymock.EasyMock
+import org.eclipse.jetty.ee10.servlet.ServletChannel
+import org.eclipse.jetty.ee10.servlet.ServletContextHandler
 import org.eclipse.jetty.http.HttpField
 import org.eclipse.jetty.http.HttpFields
-import org.eclipse.jetty.http.MetaData
-import org.eclipse.jetty.server.HttpChannel
+import org.eclipse.jetty.http.HttpURI
+import org.eclipse.jetty.server.ConnectionMetaData
+import org.eclipse.jetty.server.HttpConfiguration
 import org.eclipse.jetty.server.Request
 import org.eclipse.jetty.server.Response
 import org.slf4j.LoggerFactory
@@ -47,15 +51,18 @@ class FilteredRequestLogTest {
         val logger = factory.build("test")
         val mockRequest = LeakyMock.niceMock<Request>()
         val mockResponse = LeakyMock.niceMock<Response>()
-        EasyMock.expect(mockRequest.requestURI).andReturn("/app/ping").anyTimes()
-        EasyMock.expect(mockRequest.timeStamp).andReturn(System.currentTimeMillis() + 200).anyTimes()
-        EasyMock
-            .expect(mockResponse.committedMetaData)
-            .andReturn(
-                MetaData.Response(null, HttpServletResponse.SC_OK, null),
-            ).anyTimes()
-        EasyMock.replay(mockRequest, mockResponse)
-        logger.log(mockRequest, mockResponse)
+        val connectionMetaData = LeakyMock.niceMock<ConnectionMetaData>()
+        EasyMock.expect(mockRequest.httpURI).andReturn(HttpURI.from("/app/ping")).anyTimes()
+        EasyMock.expect(mockRequest.headersNanoTime).andReturn(System.nanoTime() + 200_000_000).anyTimes()
+        EasyMock.expect(mockResponse.status).andReturn(HttpServletResponse.SC_OK).anyTimes()
+        EasyMock.expect(connectionMetaData.httpConfiguration).andReturn(HttpConfiguration()).anyTimes()
+        EasyMock.expect(connectionMetaData.protocol).andReturn("HTTP/1.1").anyTimes()
+        EasyMock.replay(mockRequest, mockResponse, connectionMetaData)
+        val handler = ServletContextHandler()
+        val channel = ServletChannel(handler, connectionMetaData)
+        val testRequest = TestServletContextRequest(handler, channel, mockRequest, mockResponse)
+
+        logger.log(testRequest, mockResponse)
         assertThat(events).isEmpty()
     }
 
@@ -82,15 +89,17 @@ class FilteredRequestLogTest {
         val logger = factory.build("test")
         val mockRequest = LeakyMock.niceMock<Request>()
         val mockResponse = LeakyMock.niceMock<Response>()
-        EasyMock.expect(mockRequest.requestURI).andReturn("/app/ping").anyTimes()
-        EasyMock.expect(mockRequest.timeStamp).andReturn(System.currentTimeMillis() - 300).anyTimes()
-        EasyMock
-            .expect(mockResponse.committedMetaData)
-            .andReturn(
-                MetaData.Response(null, HttpServletResponse.SC_OK, null),
-            ).anyTimes()
-        EasyMock.replay(mockRequest, mockResponse)
-        logger.log(mockRequest, mockResponse)
+        val connectionMetaData = LeakyMock.niceMock<ConnectionMetaData>()
+        EasyMock.expect(mockRequest.httpURI).andReturn(HttpURI.from("/app/ping")).anyTimes()
+        EasyMock.expect(mockRequest.headersNanoTime).andReturn(System.nanoTime() - 300_000_000).anyTimes()
+        EasyMock.expect(mockResponse.status).andReturn(HttpServletResponse.SC_OK).anyTimes()
+        EasyMock.expect(connectionMetaData.httpConfiguration).andReturn(HttpConfiguration()).anyTimes()
+        EasyMock.expect(connectionMetaData.protocol).andReturn("HTTP/1.1").anyTimes()
+        EasyMock.replay(mockRequest, mockResponse, connectionMetaData)
+        val handler = ServletContextHandler()
+        val channel = ServletChannel(handler, connectionMetaData)
+        val testRequest = TestServletContextRequest(handler, channel, mockRequest, mockResponse)
+        logger.log(testRequest, mockResponse)
         assertThat(events).isNotEmpty()
     }
 
@@ -115,24 +124,26 @@ class FilteredRequestLogTest {
                 },
             )
         val logger = factory.build("test")
+
+        val fakeRequestId = "f74663ea-4da5-4890-91a7-e63f8d1ba82c"
+        val responseHeaders = HttpFields.build(HttpFields.from(HttpField(null, "X-Request-Id", fakeRequestId)))
         val mockRequest = LeakyMock.niceMock<Request>()
         val mockResponse = LeakyMock.niceMock<Response>()
-        val fakeRequestId = "f74663ea-4da5-4890-91a7-e63f8d1ba82c"
-        val fields = HttpFields.build(HttpFields.from(HttpField(null, "X-Request-Id", fakeRequestId)))
-        EasyMock.expect(mockRequest.requestURI).andReturn("/app/ping").anyTimes()
-        EasyMock.expect(mockRequest.timeStamp).andReturn(System.currentTimeMillis() + 200).anyTimes()
-        EasyMock
-            .expect(mockResponse.committedMetaData)
-            .andReturn(
-                MetaData.Response(null, HttpServletResponse.SC_SERVICE_UNAVAILABLE, fields),
-            ).anyTimes()
-        val mockChannel = LeakyMock.niceMock<HttpChannel>()
-        EasyMock.expect(mockResponse.httpChannel).andReturn(mockChannel).anyTimes()
-        EasyMock.expect(mockResponse.getHeader("X-Request-Id")).andReturn(fakeRequestId).anyTimes()
-        EasyMock.expect(mockResponse.httpFields).andReturn(fields).anyTimes()
-        EasyMock.replay(mockRequest, mockResponse)
+        val connectionMetaData = LeakyMock.niceMock<ConnectionMetaData>()
+        EasyMock.expect(mockRequest.httpURI).andReturn(HttpURI.from("/app/ping")).anyTimes()
+        EasyMock.expect(mockRequest.headersNanoTime).andReturn(System.nanoTime() + 200_000_000).anyTimes()
+        EasyMock.expect(mockRequest.headers).andReturn(HttpFields.build()).anyTimes()
+        EasyMock.expect(mockResponse.headers).andReturn(responseHeaders).anyTimes()
+        EasyMock.expect(mockResponse.status).andReturn(HttpServletResponse.SC_SERVICE_UNAVAILABLE).anyTimes()
+        EasyMock.expect(connectionMetaData.httpConfiguration).andReturn(HttpConfiguration()).anyTimes()
+        EasyMock.expect(connectionMetaData.protocol).andReturn("HTTP/1.1").anyTimes()
+        EasyMock.expect(mockRequest.connectionMetaData).andReturn(connectionMetaData).anyTimes()
+        EasyMock.replay(mockRequest, mockResponse, connectionMetaData)
+        val handler = ServletContextHandler()
+        val channel = ServletChannel(handler, connectionMetaData)
+        val testRequest = TestServletContextRequest(handler, channel, mockRequest, mockResponse)
 
-        logger.log(mockRequest, mockResponse)
+        logger.log(testRequest, mockResponse)
         assertThat(events).isNotEmpty()
 
         val context = (LoggerFactory.getLogger("http.request") as Logger).loggerContext
@@ -166,8 +177,17 @@ class FilteredRequestLogTest {
         val logger = factory.build("test")
         val mockRequest = LeakyMock.niceMock<Request>()
         val mockResponse = LeakyMock.niceMock<Response>()
-        EasyMock.replay(mockRequest, mockResponse)
-        logger.log(mockRequest, mockResponse)
+        val connectionMetaData = LeakyMock.niceMock<ConnectionMetaData>()
+        EasyMock.expect(mockRequest.httpURI).andReturn(HttpURI.from("/app/random/uri")).anyTimes()
+        EasyMock.expect(mockRequest.headersNanoTime).andReturn(System.nanoTime() + 200_000_000).anyTimes()
+        EasyMock.expect(mockResponse.status).andReturn(HttpServletResponse.SC_OK).anyTimes()
+        EasyMock.expect(connectionMetaData.httpConfiguration).andReturn(HttpConfiguration()).anyTimes()
+        EasyMock.expect(connectionMetaData.protocol).andReturn("HTTP/1.1").anyTimes()
+        EasyMock.replay(mockRequest, mockResponse, connectionMetaData)
+        val handler = ServletContextHandler()
+        val channel = ServletChannel(handler, connectionMetaData)
+        val testRequest = TestServletContextRequest(handler, channel, mockRequest, mockResponse)
+        logger.log(testRequest, mockResponse)
         assertThat(events).isNotEmpty()
     }
 }
