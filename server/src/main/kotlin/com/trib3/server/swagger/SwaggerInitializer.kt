91dbb31615de89b1caadaee0b248886373c0eceb
Joe Barnett <joseph.levi.barnett@gmail.com>
2026-05-07 15:25:14 -0700

    Fix constructor injection / default value error
    Guice injects an empty string instead of using the default value,
    so have a separate @Inject constructor instead gives the intended
    behavior.

--- server/src/main/kotlin/com/trib3/server/swagger/SwaggerInitializer.kt
+++ server/src/main/kotlin/com/trib3/server/swagger/SwaggerInitializer.kt
@@ -32,34 +32,41 @@ interface JaxrsAppProcessor {
  * so that the admin-side servlet can serve documentation for our
  * application-side Jersey resources.
  */
-class SwaggerInitializer
+class SwaggerInitializer(
+    val contextId: String,
+    val appConfig: TribeApplicationConfig,
+    val objectMapper: ObjectMapper,
+) : JaxrsAppProcessor {
     @Inject
     constructor(
-        val contextId: String =
-            OpenApiContext.OPENAPI_CONTEXT_ID_PREFIX + "servlet." +
-                OpenApiServlet::class.simpleName,
-        val appConfig: TribeApplicationConfig,
-        val objectMapper: ObjectMapper,
-    ) : JaxrsAppProcessor {
-        override fun process(application: Application) {
-            ModelConverters.getInstance().addConverter(ModelResolver(objectMapper))
-            val hostAndPath = UriBuilder.newInstance().host(appConfig.corsDomains[0]).path(appConfig.appContextPath)
-            val baseUrls =
-                listOf(
-                    hostAndPath.clone().scheme("https"),
-                    hostAndPath.clone().scheme("http"),
-                    hostAndPath.clone().scheme("http").port(appConfig.appPort),
-                )
-            SwaggerContextBuilder()
-                .openApiConfiguration(
-                    SwaggerConfiguration()
-                        .openAPI(
-                            OpenAPI().servers(
-                                baseUrls.map { Server().url(it.build().toString()) },
-                            ),
-                        ).scannerClass(JaxrsApplicationScanner::class.qualifiedName),
-                ).application(application)
-                .ctxId(contextId)
-                .buildContext(true)
-        }
+        appConfig: TribeApplicationConfig,
+        objectMapper: ObjectMapper,
+    ) : this(
+        OpenApiContext.OPENAPI_CONTEXT_ID_PREFIX + "servlet." +
+            OpenApiServlet::class.simpleName,
+        appConfig,
+        objectMapper,
+    )
+
+    override fun process(application: Application) {
+        ModelConverters.getInstance().addConverter(ModelResolver(objectMapper))
+        val hostAndPath = UriBuilder.newInstance().host(appConfig.corsDomains[0]).path(appConfig.appContextPath)
+        val baseUrls =
+            listOf(
+                hostAndPath.clone().scheme("https"),
+                hostAndPath.clone().scheme("http"),
+                hostAndPath.clone().scheme("http").port(appConfig.appPort),
+            )
+        SwaggerContextBuilder()
+            .openApiConfiguration(
+                SwaggerConfiguration()
+                    .openAPI(
+                        OpenAPI().servers(
+                            baseUrls.map { Server().url(it.build().toString()) },
+                        ),
+                    ).scannerClass(JaxrsApplicationScanner::class.qualifiedName),
+            ).application(application)
+            .ctxId(contextId)
+            .buildContext(true)
     }
+}
